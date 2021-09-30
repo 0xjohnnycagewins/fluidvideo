d1444857e757d4cf2c36e054f6eb684bf602a531
Jean-Francois Cartier <2409861+cartierjf@users.noreply.github.com>
2021-09-30 17:31:11 -0400

    video frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import videojs from "video.js";
+import VREPlayer from "videojs-react-enhanced";
+import "video.js/dist/video-js.css";
 
-function App() {
+export const App: React.FunctionComponent = () => {
+  const playerOptions: VREPlayer.IPlayerOptions = {
+    src: "https://cdn.livepeer.com/hls/1bc1nsg0kzt7mtjn/index.m3u8",
+    controls: true,
+    autoplay: "play",
+    width: 1920,
+    height: 1080,
+  };
+  const videojsOptions: VREPlayer.IVideoJsOptions = {
+    fluid: true,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ paddingLeft: 32, paddingRight: 32 }}>
+      <p>{"Video player test"}</p>
+      <VREPlayer
+        playerOptions={playerOptions}
+        videojsOptions={videojsOptions}
+        onReady={(player) => console.log(player)}
+        onPlay={() => console.log("Play!")}
+        onPause={() => console.log("Pause!")}
+        onEnded={() => console.log("Ended!")}
+      />
     </div>
   );
-}
-
-export default App;
+};
