e19a898f131fcd60320ff7f820c6fc228e2aa05a
Gabriel Cartier <gcartier@samsao.co>
2021-10-01 15:03:07 -0400

    Cleaned config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,31 @@
-import React from "react";
-import videojs from "video.js";
-import VREPlayer from "videojs-react-enhanced";
-import "video.js/dist/video-js.css";
+import React from 'react';
+import videojs from 'video.js';
+import VREPlayer from 'videojs-react-enhanced';
+import 'video.js/dist/video-js.css';
 
 export const App: React.FunctionComponent = () => {
   const playerOptions: VREPlayer.IPlayerOptions = {
-    src: "https://cdn.livepeer.com/hls/1bc1nsg0kzt7mtjn/index.m3u8",
+    src: 'https://cdn.livepeer.com/hls/1bc1nsg0kzt7mtjn/index.m3u8',
     controls: true,
-    autoplay: "play",
+    autoplay: 'play',
     width: 1920,
     height: 1080,
   };
+
   const videojsOptions: VREPlayer.IVideoJsOptions = {
     fluid: true,
   };
 
   return (
     <div style={{ paddingLeft: 32, paddingRight: 32 }}>
-      <p>{"Video player test"}</p>
+      <p>{'Video player test'}</p>
       <VREPlayer
         playerOptions={playerOptions}
         videojsOptions={videojsOptions}
         onReady={(player) => console.log(player)}
-        onPlay={() => console.log("Play!")}
-        onPause={() => console.log("Pause!")}
-        onEnded={() => console.log("Ended!")}
+        onPlay={() => console.log('Play!')}
+        onPause={() => console.log('Pause!')}
+        onEnded={() => console.log('Ended!')}
       />
     </div>
   );
