dfc1ce38a2441ad6a3574cd92dd96f2cf30c4138
Gabriel Cartier <gcartier@samsao.co>
2021-10-03 19:22:11 -0400

    Started working on connect button

--- frontend/src/components/wallet-connect-button.tsx
+++ frontend/src/components/wallet-connect-button.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@mui/material';
+import { APP_NAME, DEFAULT_NETWORK } from 'constants';
 import React from 'react';
+import WalletLink from 'walletlink';
 import Web3 from 'web3';
 
 interface Props {
@@ -11,6 +13,14 @@ export const WalletConnectButton: React.FunctionComponent<Props> = ({
   web3,
   onWalletConnected,
 }) => {
+  const defaultNetwork = DEFAULT_NETWORK();
+  const walletLink = new WalletLink({
+    appName: APP_NAME,
+  });
+  const walletLinkProvider = walletLink.makeWeb3Provider(
+    defaultNetwork.rpcUrl,
+    defaultNetwork.chainId,
+  );
   const connectWallet = (): void => {
     const i = 0;
     if (i > 1) {
